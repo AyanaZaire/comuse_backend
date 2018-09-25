8e42bdd96c2035019787946386e2f79c73ab56c7
github username <github email address>
2018-09-24 21:46:52 -0400

    updated section serializer and enrolled controller working on frontend mvp

--- app/serializers/section_serializer.rb
+++ app/serializers/section_serializer.rb
@@ -1,8 +1,17 @@
 class SectionSerializer < ActiveModel::Serializer
   attributes :id, :title, :duration, :category_id, :teacher_id, :description, :location, :price, :materials_provided, :materials_to_bring, :faqs
 
+  # belongs_to :category
+  # belongs_to :teacher, class_name: 'Member'
+  # has_many :students, class_name: 'Member'
+  # has_many :students, through: :enrolled, class_name: 'Member'
+
   belongs_to :category
+
   belongs_to :teacher, class_name: 'Member'
-  has_many :students, class_name: 'Member'
+
+  has_many :enrolled
+
+  # has_many :students, class_name: 'Member'
   has_many :students, through: :enrolled, class_name: 'Member'
 end
