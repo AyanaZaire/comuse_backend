a852369b2b1a34cacb4af4f93ff6ec3dd437d859
AyanaZaire <ayanazaire@gmail.com>
2019-02-16 16:08:32 -0500

    raise errors in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'https://comuse.herokuapp.com/' }
   # Setup for production - deliveries, no errors raised
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.perform_caching = false
