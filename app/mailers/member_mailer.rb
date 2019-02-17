04218467cf62111079462ca19a2125ea60f223fc
AyanaZaire <ayanazaire@gmail.com>
2019-02-16 23:03:23 -0500

    debugging

--- app/mailers/member_mailer.rb
+++ app/mailers/member_mailer.rb
@@ -1,29 +1,29 @@
-require 'sendgrid-ruby'
-include SendGrid
+# require 'sendgrid-ruby'
+# include SendGrid
 
 class MemberMailer < ApplicationMailer
 
   default from: 'comuse@gmail.com'
 
-  # def welcome_email(member)
-  #   # getting member from create method in member controller
-  #   @member = member
-  #   @url = 'https://comuse-backend.herokuapp.com/api/v1/login'
-  #   mail(to: @member.email, subject: 'Welcome to Co.muse')
-  # end
-
-  def send_task_complete_email
-    from = Email.new(email: 'comuse@gmail.com')
-    to = Email.new(email: 'ayanazaire@gmail.com')
-    subject = 'Sending with SendGrid is Fun'
-    content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
-    mail = SendGrid::Mail.new(from, subject, to, content)
-
-    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-    response = sg.client.mail._('send').post(request_body: mail.to_json)
-    puts response.status_code
-    puts response.body
-    puts response.headers
+  def welcome_email(member)
+    # getting member from create method in member controller
+    @member = member
+    @url = 'https://comuse-backend.herokuapp.com/api/v1/login'
+    mail(to: @member.email, subject: 'Welcome to Co.muse')
   end
+
+  # def send_task_complete_email
+  #   from = Email.new(email: 'comuse@gmail.com')
+  #   to = Email.new(email: 'ayanazaire@gmail.com')
+  #   subject = 'Sending with SendGrid is Fun'
+  #   content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+  #   mail = SendGrid::Mail.new(from, subject, to, content)
+  #
+  #   sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+  #   response = sg.client.mail._('send').post(request_body: mail.to_json)
+  #   puts response.status_code
+  #   puts response.body
+  #   puts response.headers
+  # end
 
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -35,19 +35,19 @@ Rails.application.configure do
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
-  config.action_mailer.default_url_options = { :host => ‘localhost:3000’ }
-   # config.action_mailer.default_options = { from: “comuse@gmail.com” }
-   config.action_mailer.smtp_settings = {
-   address: “smtp.gmail.com”,
-   port: 25,
-   authentication: “plain”,
-   enable_starttls_auto: true,
-   user_name: Rails.application.secrets.GMAIL_USERNAME,
-   password: Rails.application.secrets.GMAIL_PASSWORD,
-   domain: localhost:3000,
-   openssl_verify_mode: ‘none’,
-   }
+  # config.action_mailer.delivery_method = :test
+  # config.action_mailer.default_url_options = { :host => ‘localhost:3000’ }
+  #  # config.action_mailer.default_options = { from: “comuse@gmail.com” }
+  #  config.action_mailer.smtp_settings = {
+  #  address: “smtp.gmail.com”,
+  #  port: 25,
+  #  authentication: “plain”,
+  #  enable_starttls_auto: true,
+  #  user_name: Rails.application.secrets.GMAIL_USERNAME,
+  #  password: Rails.application.secrets.GMAIL_PASSWORD,
+  #  domain: localhost:3000,
+  #  openssl_verify_mode: ‘none’,
+  #  }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
