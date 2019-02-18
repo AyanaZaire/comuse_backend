f4359f968c832d9538920f1f084dcf46036576a6
AyanaZaire <ayanazaire@gmail.com>
2019-02-18 11:44:19 -0500

    debugging photo implementation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   # configuration for file upload to aws
   # tutorial: https://vimeo.com/278727014
   config.active_storage.service = :amazon_prod
-  Rails.application.routes.default_url_options[:host] = 'https://comuse-backend.herokuapp.com/rails'
+  Rails.application.routes.default_url_options[:host] = 'https://comuse-backend.herokuapp.com'
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
