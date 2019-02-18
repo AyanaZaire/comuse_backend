ae1ee7abc06e66dbc4ae7ec14a0be5a2d1e0a3eb
AyanaZaire <ayanazaire@gmail.com>
2019-02-18 15:01:24 -0500

    implementing donation calculation

--- app/controllers/api/v1/charges_controller.rb
+++ app/controllers/api/v1/charges_controller.rb
@@ -18,22 +18,40 @@ class Api::V1::ChargesController < ApplicationController
       description = params[:charges][:description]
       teacher = params[:charges][:teacher]
       teacher_stripeuid = Member.find(teacher[:id]).stripe_uid
-      donation = params[:charges][:donation]
-      puts donation
+      donation_percentage = params[:charges][:donation_percentage]
+
       #platform's cut
-      cut = ((amount * 10)/100)
-      percentage = (amount - cut)
+      # cut = ((amount * 10)/100)
+      # percentage = (amount - cut)
+
+      #platform's donation
+      platform_donation = ((amount * donation_percentage)/100)
+      amount_plus_donation = (amount + platform_donation)
+      amount_to_teacher = (amount - platform_donation)
+      puts amount
+      puts donation_percentage
+      puts platform_donation
+      puts amount_plus_donation
+      puts amount_to_teacher
 
       customer = Stripe::Customer.create(email: current_member.email, card: card_token)
 
       charge = Stripe::Charge.create({
+                  # customer: customer.id,
+                  # amount: amount,
+                  # description: description,
+                  # currency: 'usd',
+                  # # :source => card_token,
+                  # destination: {
+                  #   amount: percentage,
+                  #   account: teacher_stripeuid
                   customer: customer.id,
-                  amount: amount,
+                  amount: amount_plus_donation,
                   description: description,
                   currency: 'usd',
                   # :source => card_token,
                   destination: {
-                    amount: percentage,
+                    amount: amount_to_teacher,
                     account: teacher_stripeuid
                   }
                 })
@@ -42,7 +60,7 @@ class Api::V1::ChargesController < ApplicationController
   private
 
   def charges_params
-    params.require(:charges).permit(:stripeToken, :amount, :description, :teacher, :donation)
+    params.require(:charges).permit(:stripeToken, :amount, :description, :teacher, :donation_percentage)
   end
 
 end
