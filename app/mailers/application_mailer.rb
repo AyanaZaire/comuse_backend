a51d72bec82161bd0ac73ef0c58241069295f26e
AyanaZaire <ayanazaire@gmail.com>
2019-02-26 17:56:49 -0500

    updated default from name

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   # default from: 'comuse@gmail.com'
-  default from: 'info@comuse.co'
+  # default from: 'info@comuse.co'
+  default from: "Co.muse <info@comuse.co>"
   layout 'mailer'
 end
--- app/mailers/member_mailer.rb
+++ app/mailers/member_mailer.rb
@@ -4,7 +4,8 @@
 class MemberMailer < ApplicationMailer
 
   # default from: 'comuse@gmail.com'
-  default from: 'info@comuse.co'
+  # default from: 'info@comuse.co'
+  default from: "Co.muse <info@comuse.co>"
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def welcome_email(member)
